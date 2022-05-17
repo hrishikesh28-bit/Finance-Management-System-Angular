1f009d9ac18037d40b50629f372b9e962fd72eff
Nish0997 <nishant0997@gmail.com>
2022-05-17 12:39:31 +0530

    card frontend

---
+++ src/app/activate-user/activate-user.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MyapiService } from '../myapi.service';
+
+
+@Component({
+  selector: 'app-activate-user',
+  templateUrl: './activate-user.component.html',
+  styleUrls: ['./activate-user.component.css']
+})
+export class ActivateUserComponent implements OnInit {
+
+  regid:any;
+  
+  radioVal:any;
+  user1:any;
+  cards:any
+  
+ 
+  constructor(private router:Router, private adservice:MyapiService, private aroute:ActivatedRoute,) { 
+    //this.user=new Userdetails();
+    
+  }
+  
+ 
+  ngOnInit(): void {
+ 
+ 
+    this.adservice.getAllCards().subscribe((data:any) =>{
+      console.log(data);
+      this.cards=data;
+      
+ 
+  
+      
+    }
+    );
+  }
+
+
+}
